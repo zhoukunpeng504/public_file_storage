964b69cdd77d0e87c3af50f65a011618f911b53b
zhoukunpeng504 <18749679769@163.com>
2020-08-26 17:30:36 +0800

    fastsite_release

--- fastsite_install.py
+++ fastsite_install.py
@@ -59,7 +59,6 @@ class BTApi:
     #@data POST参数，可以是字符串或字典(必需)
     #return string
     def __http_post_cookie(self,url,p_data,timeout=20):
-        import ssl,http.cookiejar
         data = urlencode(p_data).encode('utf-8')
         req = Request(url,data)
         opener = urllib2.build_opener()
@@ -73,10 +72,11 @@ class BTApi:
         url = self.__BT_PANEL + '/plugin?action=install_plugin'
         #准备POST数据
         p_data = self.__get_key_data()  #取签名
-        p_data['sName'] = 'name'
+        p_data['sName'] = name
         p_data['version'] = ver
         p_data['type'] = type
         #请求面板接口
+        print(p_data)
         result = self.__http_post_cookie(url,p_data)
         #解析JSON数据
         return json.loads(result)
@@ -206,8 +206,8 @@ if __name__ == '__main__':
     current_dir = os.path.dirname(__file__)
     current_dir = os.path.abspath(current_dir)
     os.system("mkdir -p /root/.pip")
-    with open("/root/.pip/pip.conf", "w") as f:
-        f.write()
+    # with open("/root/.pip/pip.conf", "w") as f:
+    #     f.write('')
     os.system("mkdir -p /data")
     os.system("mkdir -p /data/www")
     if not os.path.exists("/www"):
@@ -222,16 +222,16 @@ if __name__ == '__main__':
     _p = subprocess.Popen('''ifconfig docker0|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"''',
                           shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _p.wait()
-    docker0_ip = _p.stdout.read()
+    docker0_ip = _p.stdout.read().strip()
 
     _p = subprocess.Popen('''ifconfig -a|grep inet|grep -v inet6|awk '{print $2}'|tr -d "addr:"''',
                           shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _p.wait()
-    all_ip = _p.stdout.read()
-    all_ip = [i for i in all_ip.strip() if i]
+    all_ip = _p.stdout.read().strip().split()
+    all_ip = [i for i in all_ip if i]
     all_ip = list(set(["127.0.0.1"] + [i for i in all_ip if i != docker0_ip]))
     config_json='{"open": true, "token": "abaaaf7c24c5fcc73fc394e208ebda03", "limit_addr": [%s], "binds": [{"time": 1597743896.5763838, "token": "mvRaBsOZumgBxeWkSv", "status": 0}], "apps": [], "key": "GVqlpFgeSSd9WQR6", "token_crypt": "B3g0qV1sECiJmHAK03OCWz3fHEavbwKo"}' % \
-                (",".join(['"%s"'% i for i in all_ip]))
+                (", ".join(['"%s"'% i for i in all_ip]))
     with open("/www/server/panel/config/api.json", "w") as f:
       f.write(config_json)
 
@@ -246,15 +246,15 @@ if __name__ == '__main__':
     bt_key = 'B3g0qV1sECiJmHAK03OCWz3fHEavbwKo'
     bt_api = BTApi(bt_url, bt_key)
     # bt入口 用户 密码修改
-    bt_api.set_admin_path("/fastsite_bt")
-    bt_api.reset_username('fastsite')
-    bt_api.reset_passwd(secret)
+    print(bt_api.set_admin_path("/fastsite_bt"))
+    print(bt_api.reset_username('fastsite'))
+    print(bt_api.reset_passwd(secret))
     green_print('宝塔参数初始化完成')
 
     # nginx mysql  ftp 安装
     for i in ['nginx-1.18:1', 'mysql-5.6:1', 'pureftpd-1.0:0']:
-        name,ver,type = re.match("([^-]+)-([\d]+\.[\d]+):(\d)",i).groups()
-        _ = bt_api.install_plugin(name, ver, type)
+        name,ver,_type = re.match("([^-]+)-([\d]+\.[\d]+):(\d)",i).groups()
+        _ = bt_api.install_plugin(name, ver, _type)
         print(_)
 
     # 等待安装完成
